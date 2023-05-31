49f0916887d872555e06b9ebe7eee1a6ab8a525d
nico <nico.maffe89@gmail.com>
2023-05-31 14:30:30 +0200

    added haversine_torch function

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import random
 import math
 from random import seed
+import torch
 
 #import fiona
 #import geopandas as gpd
@@ -307,6 +308,24 @@ def haversine(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
     c = 2 * np.arcsin(np.sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Determines return value units.
+    return c * r
+
+def haversine_torch(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    #lon1, lat1, lon2, lat2 = np.radians([lon1, lat1, lon2, lat2])
+    lon1, lat1, lon2, lat2 = torch.deg2rad(lon1), torch.deg2rad(lat1), torch.deg2rad(lon2), torch.deg2rad(lat2)
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = torch.sin(dlat/2)**2 + torch.cos(lat1) * torch.cos(lat2) * torch.sin(dlon/2)**2
+    c = 2 * torch.arcsin(torch.sqrt(a))
     r = 6371 # Radius of earth in kilometers. Determines return value units.
     return c * r
 
