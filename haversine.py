9827e8819e43e7b57c694bbb04759439ad980b1a
nico <nico.maffe89@gmail.com>
2023-05-31 11:19:19 +0200

    comment haversine function that operates on torch tensors

--- haversine.py
+++ haversine.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -13,5 +14,23 @@ def haversine(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
     c = 2 * np.arcsin(np.sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Determines return value units.
+    return c * r
+
+def haversine_torch(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    #lon1, lat1, lon2, lat2 = np.radians([lon1, lat1, lon2, lat2])
+    lon1, lat1, lon2, lat2 = torch.deg2rad(lon1), torch.deg2rad(lat1), torch.deg2rad(lon2), torch.deg2rad(lat2)
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = torch.sin(dlat/2)**2 + torch.cos(lat1) * torch.cos(lat2) * torch.sin(dlon/2)**2
+    c = 2 * torch.arcsin(torch.sqrt(a))
     r = 6371 # Radius of earth in kilometers. Determines return value units.
     return c * r
