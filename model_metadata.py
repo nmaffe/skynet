2e1c412b26679faaecc62c2c2b6ce5b178a9cbe1
nico <nico.maffe89@gmail.com>
2023-09-13 11:54:21 +0200

    updates

--- model_metadata.py
+++ model_metadata.py
@@ -1,13 +1,15 @@
 import time
 import copy
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+
 import pandas as pd
 import geopandas as gpd
 from scipy import stats
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, QuantileTransformer
 
 import torch
 import torch.nn as nn
@@ -15,20 +17,25 @@ from torch.optim import Adam
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+def my_loss(output, target):
+    loss = torch.mean((output - target)**4)
+    return loss
+
 class CFG:
-    cols = ['RGI', 'POINT_LAT', 'POINT_LON', 'THICKNESS', 'Area', 'elevation_astergdem',
-            'slope_lat', 'slope_lon', 'vx', 'vy', 'dist_from_border_km', 'v', 'slope',
-            'Zmin', 'Zmax', 'Zmed', 'Slope', 'Lmax', 'elevation_from_zmin', 'ith_m', 'ith_f']
 
     features = ['Area', 'slope_lat', 'slope_lon', 'elevation_astergdem', 'vx', 'vy',
-            'dist_from_border_km', 'v', 'slope', 'Zmin', 'Zmax', 'Zmed', 'Slope', 'Lmax', 'elevation_from_zmin']
+     'dist_from_border_km', 'v', 'slope', 'Zmin', 'Zmax', 'Zmed', 'Slope', 'Lmax',
+     'elevation_from_zmin', 'RGI']
+
     millan = 'ith_m'
     farinotti = 'ith_f'
     target = 'THICKNESS'
-    batch_size = 2048
+    batch_size = 64
     num_workers = 16
     lr = 0.001
-    epochs = 25
+    epochs = 100
+    loss = nn.MSELoss()# my_loss #nn.L1Loss() #nn.MSELoss() # nn.L1Loss()
+    L2_penalty=0.000
 
 # ====================================================
 # Model
@@ -36,13 +43,67 @@ class CFG:
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
+
+        self.dropout = nn.Dropout(p=0.5)
+
         self.fc1 = nn.Linear(len(CFG.features), 100)
-        self.fc2 = nn.Linear(100, 1)
+        self.fc1_1 = nn.Linear(50, 100)
+        self.fc1_2 = nn.Linear(100, 20)
+        self.fc1_3 = nn.Linear(100, 1)
+
+        self.fc2 = nn.Linear(len(CFG.features), 50)
+        self.fc2_1 = nn.Linear(50, 100)
+        self.fc2_2 = nn.Linear(100, 20)
+        self.fc2_3 = nn.Linear(20, 1)
+
+        self.fc_A = nn.Linear(3, 10)
+        self.fcA_1 = nn.Linear(10, 1)
+
+        self.fc3 = nn.Linear(len(CFG.features), 100)
+        self.fc3_1 = nn.Linear(100, 1)
     def forward(self, x, m, f):
-        x = torch.relu(self.fc1(x))
-        #x = torch.relu(self.fc2(x))
-        x = self.fc2(x)
-        return x
+        x1 = torch.relu(self.fc1(x))
+        x1 = self.dropout(x1)
+        #x1 = torch.relu(self.fc1_1(x1))
+        #x1 = self.dropout(x1)
+        #x1 = torch.relu(self.fc1_2(x1))
+        #x1 = self.dropout(x1)
+        x1 = self.fc1_3(x1)
+        #x1 = torch.sigmoid(self.fc1_3(x1))
+        #print('x1:', x1.shape)
+
+        #x1 = torch.add(x1, m)
+        #x1 = torch.add(x1, f)
+
+        #x2 = torch.relu(self.fc2(x))    # (N,100)
+        #x2 = self.dropout(x2)
+        #x2 = torch.relu(self.fc2_1(x2))
+        #x2 = self.dropout(x2)
+        #x2 = torch.relu(self.fc2_2(x2))
+        #x2 = self.dropout(x2)
+        #x2 = self.fc2_3(x2)
+        #x2 = torch.sigmoid(self.fc2_3(x2))             # (N,1)
+        #print('x2:', x2.shape)
+
+        #xm = torch.mul(m, x1)           # (N,1)
+        #xf = torch.mul(f, x2)           # (N,1)
+        #print('xm:', xm.shape)
+        #print('xf:', xf.shape)
+
+        #x_physics = torch.add(xm, xf)
+        x_physics = torch.concat((m, f, x1), 1)#((xm, xf), 1)
+        x_physics = torch.relu(self.fc_A(x_physics))
+        x_physics = self.fcA_1(x_physics)
+
+        #x3 = torch.relu(self.fc3(x))
+        #x3 = self.fc3_1(x3)
+
+        #x_out = torch.add(x_physics, x3)
+
+        #print('x:', x.shape)
+        #input('wait')
+
+        return x_physics
 
 # ====================================================
 # Dataset
@@ -73,43 +134,24 @@ class MaffeDataset(Dataset):
         return xfeatures, target, millan, farinotti
 
 
-PATH_METADATA = '/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final.csv'
+PATH_METADATA = '/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/'
+file = 'TTT_final_grid_20.csv'
 
-glathida = pd.read_csv(PATH_METADATA, low_memory=False)
+glathida_rgis = pd.read_csv(PATH_METADATA+file, low_memory=False)
+print(f'Dataset: {len(glathida_rgis)} rows.')
 
-# Add features
-glathida['v'] = np.sqrt(glathida['vx']**2 + glathida['vy']**2)
-glathida['slope'] = np.sqrt(glathida['slope_lat']**2 + glathida['slope_lon']**2)
-glathida['elevation_from_zmin'] = glathida['elevation_astergdem'] - glathida['Zmin']
-
-rgis = [3, 7, 8, 11]
-
-cond = ((glathida['RGI'].isin(rgis)) & (glathida['SURVEY_DATE'] > 20050000)
-        & (glathida['DATA_FLAG'].isna()) & (glathida['THICKNESS']>=0)
-        & (glathida['ith_m']>=0) & (glathida['ith_f']>=0))
-
-glathida_rgis = glathida[cond]
-print('here', list(glathida_rgis))
-
-# Keep only these columns
-glathida_rgis = glathida_rgis[CFG.cols]
-
-# Cast all columns to float32: (probably useless)
-glathida_rgis = glathida_rgis.astype('float32')
-
-# Shuffle (probably useless)
-glathida_rgis = glathida_rgis.sample(frac = 1)
-
-# Remove nans
-glathida_rgis = glathida_rgis.dropna(subset=CFG.cols)
-print(f'After having removed nans we have {len(glathida_rgis)} rows')
-print(glathida_rgis['RGI'].value_counts())
 
 # -----------------------------------------------------------
 # Train and Test
-val = glathida_rgis.loc[(glathida_rgis['RGI']==3) & (glathida_rgis['POINT_LAT']<76)]
-train = glathida_rgis.drop(val.index)
-test = copy.deepcopy(val)
+#test = glathida_rgis.loc[(glathida_rgis['RGI']==3) & (glathida_rgis['POINT_LAT']<76)]
+test = glathida_rgis.loc[(glathida_rgis['RGI']==3) & (glathida_rgis['POINT_LAT']<76)].sample(n=1879)
+#val = glathida_rgis.loc[(glathida_rgis['RGI']==3)].drop(test.index).sample(n=2000)
+val = glathida_rgis.loc[(glathida_rgis['RGI']==3)].drop(test.index).sample(n=2000)
+#train = glathida_rgis.sample(100000)#.drop(val.index)
+#train = glathida_rgis.drop(val.index).sample(500000)
+train = glathida_rgis.loc[(glathida_rgis['RGI']==3)].drop(test.index).drop(val.index)
+
+print(f'Train: {len(train)}, Val: {len(val)}, Test: {len(test)}')
 
 # ====================================================
 # model & optimizer
@@ -120,28 +162,28 @@ print(f'working with {device}')
 model = NeuralNetwork()
 model.to(device)
 
-optimizer = Adam(model.parameters(), lr=CFG.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.001)
+optimizer = Adam(model.parameters(), lr=CFG.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=CFG.L2_penalty)
 
 # ====================================================
 # Train loop
 # ====================================================
-def train_loop(model, optimizer):
+def train_loop(train, val, model, optimizer):
 
     best_mse = np.inf
     best_weights = None
 
     # Datasets
     train_dataset = MaffeDataset(train, transform=None)
-    test_dataset = MaffeDataset(val, transform=None)
-    print(f'Train: {len(train_dataset)}, Test: {len(test_dataset)}')
+    val_dataset = MaffeDataset(val, transform=None)
+    print(f'Train: {len(train_dataset)}, Val: {len(val_dataset)}')
 
     # Dataloaders
     train_loader = DataLoader(train_dataset, batch_size=CFG.batch_size, shuffle=True,
                                   num_workers=CFG.num_workers, drop_last=True)
-    test_loader = DataLoader(test_dataset, batch_size=CFG.batch_size, shuffle=False,
+    val_loader = DataLoader(val_dataset, batch_size=CFG.batch_size, shuffle=False,
                               num_workers=CFG.num_workers, drop_last=True)
 
-    criterion = nn.MSELoss()
+    criterion = CFG.loss
 
     # ====================================================
     # loop
@@ -150,25 +192,28 @@ def train_loop(model, optimizer):
     print('Start training loop.')
     since = time.time()
 
-    train_loss_history, test_loss_history = [], []
-    train_metrics_history, test_metrics_history = [], []
+    train_loss_history, val_loss_history = [], []
+    train_metrics_history, val_metrics_history = [], []
 
     for epoch in range(CFG.epochs):
 
-        train_loss_epoch, test_loss_epoch = [], []
-        train_metrics_epoch, test_metrics_epoch = [], []
+        train_loss_epoch, val_loss_epoch = [], []
+        train_metrics_epoch, val_metrics_epoch = [], []
 
         model.train()
         for step, (X_train, y_train, y_train_m, y_train_f) in enumerate(train_loader):
 
             X_train = X_train.to(device)    #(N,15)
             y_train = y_train.reshape(-1, 1).to(device)    # (N,1)
-            #print(type(X_train), X_train.shape, type(y_train), y_train.shape, X_train.dtype)
+            y_train_m = y_train_m.reshape(-1, 1).to(device)
+            y_train_f = y_train_f.reshape(-1, 1).to(device)
+            #print(type(X_train), X_train.shape, type(y_train), y_train.shape, X_train.dtype, type(y_train_m), y_train_m.shape)
+            #input('wait')
 
             y_preds = model(X_train, y_train_m, y_train_f) # (N,1)
             loss = criterion(y_preds, y_train)
-
             #print(step, X_train.shape, y_train.shape, y_preds.shape)
+            #input('wait')
 
             r2 = r2_score(y_preds.detach().cpu().numpy(), y_train.detach().cpu().numpy())
             rmse = mean_squared_error(y_preds.detach().cpu().numpy(), y_train.detach().cpu().numpy(), squared=False)
@@ -185,9 +230,11 @@ def train_loop(model, optimizer):
         train_metrics_history.append(np.mean(train_metrics_epoch))
 
         model.eval()
-        for step, (X_test, y_test, y_test_m, y_test_f) in enumerate(test_loader):
+        for step, (X_test, y_test, y_test_m, y_test_f) in enumerate(val_loader):
             X_test = X_test.to(device)
             y_test = y_test.reshape(-1, 1).to(device)
+            y_test_m = y_test_m.reshape(-1, 1).to(device)
+            y_test_f = y_test_f.reshape(-1, 1).to(device)
             with torch.no_grad():
                 y_preds = model(X_test, y_test_m, y_test_f)
                 loss = criterion(y_preds, y_test)
@@ -195,8 +242,8 @@ def train_loop(model, optimizer):
             r2 = r2_score(y_preds.detach().cpu().numpy(), y_test.detach().cpu().numpy())
             rmse = mean_squared_error(y_preds.detach().cpu().numpy(), y_test.detach().cpu().numpy(), squared=False)
 
-            test_loss_epoch.append(float(loss))
-            test_metrics_epoch.append(rmse)
+            val_loss_epoch.append(float(loss))
+            val_metrics_epoch.append(rmse)
 
             #if (step % 1000 == 0):
             #    print('Test', epoch, step, '\t', float(loss), '\t', r2)
@@ -205,16 +252,24 @@ def train_loop(model, optimizer):
                 best_mse = float(loss)
                 best_weights = copy.deepcopy(model.state_dict())
 
-        test_loss_history.append(np.mean(test_loss_epoch))
-        test_metrics_history.append(np.mean(test_metrics_epoch))
+        val_loss_history.append(np.mean(val_loss_epoch))
+        val_metrics_history.append(np.mean(val_metrics_epoch))
 
-        print(f'Epoch {epoch} | Train loss {train_loss_history[-1]:.2f} | Val loss {test_loss_history[-1]:.2f} '
-              f'| Train rmse {train_metrics_history[-1]:.3f} | Val rmse {test_metrics_history[-1]:.3f}')
+        print(f'Epoch {epoch} | Train loss {train_loss_history[-1]:.2f} | Val loss {val_loss_history[-1]:.2f} '
+              f'| Train rmse {train_metrics_history[-1]:.3f} | Val rmse {val_metrics_history[-1]:.3f}')
 
+    print('Finished training loop.')
+    return train_loss_history, train_metrics_history, val_loss_history, val_metrics_history, best_weights
 
-    return train_loss_history, train_metrics_history, test_loss_history, test_metrics_history, best_weights
+train_loss_history, train_metrics_history, val_loss_history, val_metrics_history, best_weights = train_loop(train, val, model, optimizer)
 
-train_loss_history, train_metrics_history, test_loss_history, test_metrics_history, best_weights = train_loop(model, optimizer)
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(np.arange(0, len(train_loss_history)), train_loss_history, '--', c='r')
+ax1.plot(np.arange(0, len(val_loss_history)), val_loss_history, '--', c='g')
+ax2.plot(np.arange(0, len(train_metrics_history)), train_metrics_history, 'r-')
+ax2.plot(np.arange(0, len(val_metrics_history)), val_metrics_history, 'g-')
+plt.show()
 
 # load the best model
 best_model = NeuralNetwork()
@@ -226,12 +281,12 @@ best_model.eval()
 #test[CFG.features] = scaler1.fit_transform(test[CFG.features])
 X_test = torch.tensor(test[CFG.features].to_numpy(), dtype=torch.float32)
 y_test = test[CFG.target].to_numpy()
-y_test_m = torch.tensor(test[CFG.millan].to_numpy(), dtype=torch.float32)
-y_test_f = torch.tensor(test[CFG.farinotti].to_numpy(), dtype=torch.float32)
-#mu, devst = np.mean(y_test), np.std(y_test)
-y_preds = best_model(X_test, y_test_m, y_test_f).detach().cpu().numpy().squeeze()
-#y_preds = y_preds * devst + mu
-
+y_test_m = test[CFG.millan].to_numpy()
+y_test_f = test[CFG.farinotti].to_numpy()
+y_preds = best_model(X_test,
+                     torch.tensor(y_test_m, dtype=torch.float32).reshape(-1, 1),
+                     torch.tensor(y_test_f, dtype=torch.float32).reshape(-1, 1)).detach().cpu().numpy().squeeze()
+#print(y_test.shape, y_preds.shape, y_test_m.shape, y_test_f.shape)
 
 def evaluation(y, predictions):
     mae = mean_absolute_error(y, predictions)
@@ -248,21 +303,60 @@ def evaluation(y, predictions):
     print(f'r: {r_value}')
     return mae, mse, rmse, r_squared, slope, intercept, r_value
 
-fig, ax = plt.subplots()
-s1 = ax.plot(range(len(train_loss_history)), train_loss_history, 'o-', c='b', alpha=.3)
-s2 = ax.plot(range(len(test_loss_history)), test_loss_history, 'o-', c='r', alpha=.3)
-plt.show()
-
+# benchmarks
 nn_metrics = evaluation(y_test, y_preds)
 
-fig, (ax1, ax2) = plt.subplots(1,2)
-s1 = ax1.scatter(x=y_test, y=y_preds, s=1)
-s2 = ax1.plot([0.0, 1200], [0.0, 1200], c='r')
+# stats
+mu_ML = np.mean(y_test-y_preds)
+med_ML = np.median(y_test-y_preds)
+std_ML = np.std(y_test-y_preds)
+mu_millan = np.mean(y_test-y_test_m)
+med_millan = np.median(y_test-y_test_m)
+std_millan = np.std(y_test-y_test_m)
+mu_farinotti = np.mean(y_test-y_test_f)
+med_farinotti = np.median(y_test-y_test_f)
+std_farinotti = np.std(y_test-y_test_f)
+print(f'Benchmarks ML, Millan and Farinotti: {std_ML:.2f} {std_millan:.2f} {std_farinotti:.2f}')
+
+# fits
+m_ML, q_ML, _, _, _ = stats.linregress(y_test,y_preds)
+m_millan, q_millan, _, _, _ = stats.linregress(y_test,y_test_m)
+m_farinotti, q_farinotti, _, _, _ = stats.linregress(y_test,y_test_f)
+
+# plot
+fig, (ax1, ax2) = plt.subplots(1,2, figsize=(8,4))
+s1 = ax1.scatter(x=y_test, y=y_preds, s=5, c=None, ec='b')
+#s1 = ax1.scatter(x=y_test, y=test['ith_m'], s=5, c='g', alpha=.1)
+#s1 = ax1.scatter(x=y_test, y=test['ith_f'], s=5, c='r', alpha=.1)
+
+xmax = max(np.max(y_test), np.max(y_preds), np.max(y_test_m), np.max(y_test_f))
+
+fit_ML_plot = ax1.plot([0.0, xmax], [q_ML, q_ML+xmax*m_ML], c='b')
+fit_millan_plot = ax1.plot([0.0, xmax], [q_millan, q_millan+xmax*m_millan], c='lime')
+fit_farinotti_plot = ax1.plot([0.0, xmax], [q_farinotti, q_farinotti+xmax*m_farinotti], c='r')
+s2 = ax1.plot([0.0, xmax], [0.0, xmax], c='k')
+ax1.axis([None, xmax, None, xmax])
+
+ax2.hist(y_test-y_preds, bins=np.arange(-1000, 1000, 10), label='NN', color='lightblue', ec='blue', alpha=.4, zorder=2)
+ax2.hist(y_test-y_test_m, bins=np.arange(-1000, 1000, 10), label='Millan', color='green', ec='green', alpha=.3, zorder=1)
+ax2.hist(y_test-y_test_f, bins=np.arange(-1000, 1000, 10), label='Farinotti', color='red', ec='red', alpha=.3, zorder=1)
+
+# text
+text_ml = f'NN\n$\mu$ = {mu_ML:.1f}\nmed = {med_ML:.1f}\n$\sigma$ = {std_ML:.1f}'
+text_millan = f'Millan\n$\mu$ = {mu_millan:.1f}\nmed = {med_millan:.1f}\n$\sigma$ = {std_millan:.1f}'
+text_farinotti = f'Farinotti\n$\mu$ = {mu_farinotti:.1f}\nmed = {med_farinotti:.1f}\n$\sigma$ = {std_farinotti:.1f}'
+# text boxes
+props_ML = dict(boxstyle='round', facecolor='lightblue', ec='blue', alpha=0.4)
+props_millan = dict(boxstyle='round', facecolor='lime', ec='green', alpha=0.4)
+props_farinotti = dict(boxstyle='round', facecolor='salmon', ec='red', alpha=0.4)
+ax2.text(0.05, 0.95, text_ml, transform=ax2.transAxes, fontsize=10, verticalalignment='top', bbox=props_ML)
+ax2.text(0.05, 0.7, text_millan, transform=ax2.transAxes, fontsize=10, verticalalignment='top', bbox=props_millan)
+ax2.text(0.05, 0.45, text_farinotti, transform=ax2.transAxes, fontsize=10, verticalalignment='top', bbox=props_farinotti)
+
 ax1.set_xlabel('Glathida ice thickness (m)')
 ax1.set_ylabel('Modelled ice thickness (m)')
+ax2.set_xlabel('Glathida - Model (m)')
+ax2.legend(loc='best')
 
-ax2.hist(y_test-y_preds, bins=np.arange(-1000, 1000, 100), color='lightblue', ec='blue', alpha=.5)
-ax2.text(0.75, 0.9, f'Mean: {np.mean(y_test-y_preds):.2f}', transform = ax2.transAxes)
-ax2.text(0.75, 0.85, f'Std: {np.std(y_test-y_preds):.0f}', transform = ax2.transAxes)
-ax2.set_xlabel('Glathida - ML model (m)')
+plt.tight_layout()
 plt.show()
