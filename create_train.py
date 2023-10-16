e6f90558264762fe46a48cc8039b3cbc8ff3ef66
nico <nico.maffe89@gmail.com>
2023-10-16 17:11:19 +0200

    higher resolution: 256 -> 384

--- create_train.py
+++ create_train.py
@@ -11,17 +11,16 @@ from set_generation import flow_train_dataset
 parser = argparse.ArgumentParser(description='Create training images')
 parser.add_argument("--input",  type=str, default=None, help="input DEM mosaic file")
 parser.add_argument("--outdir", type=str, default="dataset/", help="path for the output files")
-
 parser.add_argument("--region",  type=str, default=None, help="RGI region")
-parser.add_argument("--shape",  type=int, default=256, help="size of train patches")
+parser.add_argument("--shape",  type=int, default=384, help="size of train patches") # 256
 parser.add_argument("--version",  type=str, default='62', help="RGI version")
 parser.add_argument("--epsg",  type=str, default="EPSG:4326", help="DEM projection")
 
 # adjust naming and sampling behavior 
 parser.add_argument("--max_height",  type=int, default=9999, help="Max desired height of training samples")
-parser.add_argument("--threshold",  type=int, default=2000, help="Threshold value to sample high elevation regions")
+parser.add_argument("--threshold",  type=int, default=2500, help="Threshold value to sample high elevation regions")
 parser.add_argument("--mode",  type=str, default='average', help="Threshold mode: average or max")
-parser.add_argument("--samples",  type=int, default=40000, help="Number of samples to attempt to create")
+parser.add_argument("--samples",  type=int, default=40, help="Number of samples to attempt to create") # 40000
 parser.add_argument("--postfix",  type=str, default='', help="postfix added behind samples")
 
 
@@ -60,9 +59,9 @@ def main():
     dem = rioxarray.open_rasterio(args.input).squeeze()
     if (args.region == '13' or args.region == '14' or args.region == '15'):
         print('merging the three mosaic masks 13-14-15...')
-        m13 = rioxarray.open_rasterio('/home/nico/PycharmProjects/skynet/ASTERDEM_v3_mosaics/mosaic_RGI_13_mask.tif')
-        m14 = rioxarray.open_rasterio('/home/nico/PycharmProjects/skynet/ASTERDEM_v3_mosaics/mosaic_RGI_14_mask.tif')
-        m15 = rioxarray.open_rasterio('/home/nico/PycharmProjects/skynet/ASTERDEM_v3_mosaics/mosaic_RGI_15_mask.tif')
+        m13 = rioxarray.open_rasterio('/media/nico/samsung_nvme/ASTERDEM_v3_mosaics/mosaic_RGI_13_mask.tif')
+        m14 = rioxarray.open_rasterio('/media/nico/samsung_nvme/ASTERDEM_v3_mosaics/mosaic_RGI_14_mask.tif')
+        m15 = rioxarray.open_rasterio('/media/nico/samsung_nvme/ASTERDEM_v3_mosaics/mosaic_RGI_15_mask.tif')
         empty = (m13 + m14 + m15).squeeze()
     else:
         empty = rioxarray.open_rasterio(args.input.replace('.tif', '_mask.tif')).squeeze()
