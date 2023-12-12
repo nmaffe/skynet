data normalization: input img changed to fixed [0,9000] -> [-1,1]

Deepfillv2/libs/data.py
@@ -259,7 +259,7 @@ class ImageDataset_box(Dataset):
 
         # Normalize dem
         # normalize to [0, 1] and scale to [-1, 1]
-        img_max = torch.max(img)  #9000.0
+        img_max = 9000.0 #torch.max(img)  #9000.0
         img_min = 0.0  # torch.min(img)
         img = (img - img_min) / (img_max - img_min) # note that this can cause nans
         img.mul_(2).sub_(1) # (1, 256, 256)