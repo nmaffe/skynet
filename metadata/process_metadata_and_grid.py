30e7699eb7c86fe9db94d56a515fc81b8bfdb327
nico <nico.maffe89@gmail.com>
2023-12-17 00:41:15 +0100

    updates

--- metadata/process_metadata_and_grid.py
+++ metadata/process_metadata_and_grid.py
@@ -5,7 +5,42 @@ import pandas as pd
 import geopandas as gpd
 from scipy import stats
 
-PATH_IN = '/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final.csv'
-glathida = pd.read_csv(PATH_IN, low_memory=False)
-glathida.describe().T
+PATH_METADATA = '/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final.csv'
+glathida = pd.read_csv(PATH_METADATA, low_memory=False)
 print(list(glathida))
+
+""" A. Work on the dataset """
+
+# A.1 Add features
+glathida['v'] = np.sqrt(glathida['vx']**2 + glathida['vy']**2)
+glathida['slope'] = np.sqrt(glathida['slope_lat']**2 + glathida['slope_lon']**2)
+glathida['elevation_from_zmin'] = glathida['elevation_astergdem'] - glathida['Zmin']
+
+# A.2 Select only regions of interest, remove old (-er than 2005) measurements
+# and erroneous data (if DATA_FLAG is not nan)
+rgis = [3,7,8,11]
+
+cond = ((glathida['RGI'].isin(rgis)) & (glathida['SURVEY_DATE'] > 20050000)
+        & (glathida['DATA_FLAG'].isna()) & (glathida['THICKNESS']>=0)) #& (glathida['ith_m']>0)
+
+glathida_rgis = glathida[cond]
+print(f'Original columns: {list(glathida_rgis)} \n')
+
+# A.3 Keep only these columns
+cols = ['RGI', 'RGIId', 'POINT_LAT', 'POINT_LON', 'THICKNESS', 'Area', 'elevation_astergdem',
+        'slope_lat', 'slope_lon', 'vx', 'vy', 'dist_from_border_km', 'v', 'slope',
+       'Zmin', 'Zmax', 'Zmed', 'Slope', 'Lmax', 'elevation_from_zmin', 'ith_m', 'ith_f']
+
+glathida_rgis = glathida_rgis[cols]
+#print(f'We keep only the following columns: \n {list(glathida_rgis)} \n{len(glathida_rgis)} rows')
+
+# A.4 Remove nans
+glathida_rgis = glathida_rgis.dropna(subset=cols)
+#print(glathida_rgis.isna().sum())
+#print(f'After having removed nans we have {len(glathida_rgis)} rows')
+#print(glathida_rgis['RGI'].value_counts())
+
+""" B. Grid the dataset """
+# In the previous cell we first include RGIId as feature, discarding those measurements that do not have such id.
+# We then loop over all unique glacier ids.
+# For each glacier we grid each feature.
