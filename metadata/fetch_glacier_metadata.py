b6737ad394351bf544c704f55a30b6775d968788
nico <nico.maffe89@gmail.com>
2024-01-04 03:06:33 +0100

    Clean dataframe from nan. Policy for data imputation for Millan missing data.

--- metadata/fetch_glacier_metadata.py
+++ metadata/fetch_glacier_metadata.py
@@ -20,17 +20,18 @@ import utm
 import shapely.wkt
 import time
 
-
 """
-The purpose of this program is to produce the glacier metadata 
-at for some random locations inside the glacier geometry. 
+The purpose of this program is to generate glacier metadata 
+at some random locations inside the glacier geometry. 
 
 Input: glacier name (RGIId), how many points you want to generate. 
 Output: pandas dataframe with features calculated for each generated point. 
+
+Note the following policy for Millan special cases to produce vx, vy, v, ith_m:
+    1) There is no Millan data for such glacier. I force fake vy=vy=v=0.0 and keep ith_m=nan. 
+    2) In case the interpolation of Millan's fields yields nan because points are either too close to the margins 
+    or inside a nunatak, I keep the nans that will be however removed before returning the dataset.   
 """
-# todo: check that the masks I am using to do the inpainting already account for the nunataks. That's important.
-# todo also check the same is done for create_metadata in calculating the distance from border
-# todo (=nunatak if that is the case)
 parser = argparse.ArgumentParser()
 parser.add_argument('--mosaic', type=str,default="/media/nico/samsung_nvme/ASTERDEM_v3_mosaics/",
                     help="Path to DEM mosaics")
@@ -38,7 +39,6 @@ parser.add_argument('--millan_velocity_folder', type=str,default="/home/nico/Pyc
                     help="Path to Millan velocity data")
 parser.add_argument('--millan_icethickness_folder', type=str,default="/home/nico/PycharmProjects/skynet/Extra_Data/Millan/thickness/",
                     help="Path to Millan ice thickness data")
-
 
 args = parser.parse_args()
 
@@ -120,24 +120,26 @@ def populate_glacier_with_metadata(glacier_name, n=50):
         #print("Analyzed:", analyzed)
         return neighbors
 
-    # Get glacier dataset and necessary stuff
+    # Get glacier dataset
     try:
         gl_df = oggm_rgi_glaciers.loc[oggm_rgi_glaciers['RGIId']==glacier_name]
         print(f"Glacier {glacier_name} found.")
-        print(gl_df.T)
-    except Exception as e: print(f"Error {e}")
-    assert len(gl_df) == 1, "Check this please."
+        assert len(gl_df) == 1, "Check this please."
+        # print(gl_df.T)
+    except Exception as e:
+        print(f"Error {e}")
+        exit()
 
-    # intersects of glacier
+    # intersects of glacier (need only for plotting purposes)
     gl_intersects = oggm.utils.get_rgi_intersects_entities([glacier_name], version='62')
 
-    # intersects of all glaciers in the cluster
+    # Calculate intersects of all glaciers in the cluster
     list_cluster_RGIIds = find_cluster_RGIIds(glacier_name, oggm_rgi_intersects)
-    print(f"List of glacier cluster: {list_cluster_RGIIds}")
-    if list_cluster_RGIIds is not None:
-        cluster_intersects = oggm.utils.get_rgi_intersects_entities(list_cluster_RGIIds, version='62')
-    else: cluster_intersects = None
+    # print(f"List of glacier cluster: {list_cluster_RGIIds}")
 
+    if list_cluster_RGIIds is not None:
+        cluster_intersects = oggm.utils.get_rgi_intersects_entities(list_cluster_RGIIds, version='62') # (need only for plotting purposes)
+    else: cluster_intersects = None
 
     gl_geom = gl_df['geometry'].item() # glacier geometry Polygon
     gl_geom_ext = Polygon(gl_geom.exterior)  # glacier geometry Polygon
@@ -240,10 +242,6 @@ def populate_glacier_with_metadata(glacier_name, n=50):
 
     """ Calculate Millan vx, vy, v """
     print(f"Calculating vx, vy, v, ith_m...")
-    #todo There are multiple cases I need to consider to produce vx, vy, v, ith_m
-    # 1) There is no Millan data for such glacier. I need some kind of data imputation (for vx, vy, v).
-    # 2) The point is generated close to the margin and the interpolatin of millan maps yields nan. What to do ?
-    # 3) The point is generated inside a nunatak. Millan interpolation yields nan.
 
     # get Millan vx files and create mosaic
     files_vx = glob(args.millan_velocity_folder + 'RGI-{}/VX_RGI-{}*'.format(rgi, rgi))
@@ -319,43 +317,48 @@ def populate_glacier_with_metadata(glacier_name, n=50):
             f"From Millan vx, vy, ith we have generated {np.isnan(vx_data).sum()}/{np.isnan(vy_data).sum()}/{np.isnan(ith_data).sum()} nans.")
 
         # Fill dataframe with vx, vy, ith_m
-        points_df['vx'] = vx_data  # note this may contain nans
-        points_df['vy'] = vy_data  # note this may contain nans
-        points_df['ith_m'] = ith_data  # note this may contain nans
+        points_df['vx'] = vx_data  # note this may contain nans from interpolation at the margin/inside nunatak
+        points_df['vy'] = vy_data  # note this may contain nans from interpolation at the margin/inside nunatak
+        points_df['ith_m'] = ith_data  # note this may contain nans from interpolation at the margin/inside nunatak
         points_df['v'] = np.sqrt(points_df['vx'] ** 2 + points_df['vy'] ** 2)  # note this may contain nans
 
     except:
         print(f"No Millan data can be found for rgi {rgi} glacier {glacier_name}")
-        # todo: what to do if vx, vy, ith are not defined for this glacier ? How do I fill the dataframe ?
-        # Is this triggered only if no millan data or also if point close to margin ?
-
+        for col in ['vx','vy', 'v']: # Fill Millan velocities with zero (keep ith_m as nan)
+            points_df[col] = 0.0
 
     """ Calculate distance_from_border """
-    print(f"Calculating the distances using geometry approach... ")
-    t0 = time.time()
+    print(f"Calculating the distances using glacier geometries... ")
 
     # Get the UTM EPSG code from glacier center coordinates
     cenLon, cenLat = gl_df['CenLon'].item(), gl_df['CenLat'].item()
     _, _, _, _, glacier_epsg = from_lat_lon_to_utm_and_epsg(cenLat, cenLon)
 
-    # Create Geopandas geoseries objects of all glacier geometries (boundary and nunataks) and convert to UTM
-    exterior_ring = gl_geom.exterior  # shapely.geometry.polygon.LinearRing
-    interior_rings = gl_geom.interiors  # shapely.geometry.polygon.InteriorRingSequence of polygon.LinearRing
-    geoseries_geometries_4326 = gpd.GeoSeries([exterior_ring] + list(interior_rings), crs="EPSG:4326")
-    geoseries_geometries_epsg = geoseries_geometries_4326.to_crs(epsg=glacier_epsg)
+    # Create Geopandas geoseries objects of glacier geometries (boundary and nunataks) and convert to UTM
+    if list_cluster_RGIIds is None: # Case 1: isolated glacier
+        print(f"Isolated glacier")
+        exterior_ring = gl_geom.exterior  # shapely.geometry.polygon.LinearRing
+        interior_rings = gl_geom.interiors  # shapely.geometry.polygon.InteriorRingSequence of polygon.LinearRing
+        geoseries_geometries_4326 = gpd.GeoSeries([exterior_ring] + list(interior_rings), crs="EPSG:4326")
+        geoseries_geometries_epsg = geoseries_geometries_4326.to_crs(epsg=glacier_epsg)
 
-    geoseries_geometries_cluster_intersects_4326 = cluster_intersects['geometry'] # ok but do i need this ?
-    if list_cluster_RGIIds is not None:
+    elif list_cluster_RGIIds is not None: # Case 2: cluster of glaciers with ice divides
+        print(f"Cluster of glaciers with ice divides.")
         cluster_geometry_list = []
         for gl_neighbor_id in list_cluster_RGIIds:
             gl_neighbor_df = oggm_rgi_glaciers.loc[oggm_rgi_glaciers['RGIId'] == gl_neighbor_id]
             gl_neighbor_geom = gl_neighbor_df['geometry'].item()
             cluster_geometry_list.append(gl_neighbor_geom)
 
+        # Combine into a series of all glaciers in the cluster
         cluster_geometry_4326 = gpd.GeoSeries(cluster_geometry_list, crs="EPSG:4326")
+
         # Now remove all ice divides
         cluster_geometry_no_divides_4326 = gpd.GeoSeries(cluster_geometry_4326.unary_union, crs="EPSG:4326")
         cluster_geometry_no_divides_epsg = cluster_geometry_no_divides_4326.to_crs(epsg=glacier_epsg)
+        cluster_exterior_ring = cluster_geometry_no_divides_epsg.item().exterior  # shapely.geometry.polygon.LinearRing
+        cluster_interior_rings = cluster_geometry_no_divides_epsg.item().interiors
+        geoseries_geometries_epsg = gpd.GeoSeries([cluster_exterior_ring] + list(cluster_interior_rings))
 
     # Get all generated points and create Geopandas geoseries and convert to UTM
     list_points = [Point(lon, lat) for (lon, lat) in zip(points_df['lons'], points_df['lats'])]
@@ -382,7 +385,7 @@ def populate_glacier_with_metadata(glacier_name, n=50):
             min_distance_index = min_distances_point_geometries.idxmin()
             nearest_line = geoseries_geometries_epsg.loc[min_distance_index]
             nearest_point_on_line = nearest_line.interpolate(nearest_line.project(point_epsg))
-            print(f"{i} Minimum distance: {min_dist:.2f} meters.")
+            # print(f"{i} Minimum distance: {min_dist:.2f} meters.")
 
         # Fill dataset if generated point not is not inside (any) nunatak
         if nunatak == 0: points_df.loc[i, 'dist_from_border_km'] = min_dist/1000.
@@ -391,7 +394,7 @@ def populate_glacier_with_metadata(glacier_name, n=50):
         plot_calculate_distance = True
         if plot_calculate_distance:
             fig, (ax1, ax2) = plt.subplots(1,2)
-            #ax1.plot(*gl_geom_ext.exterior.xy, lw=1, c='red')
+            ax1.plot(*gl_geom_ext.exterior.xy, lw=1, c='red')
             for interior in gl_geom.interiors:
                 ax1.plot(*interior.xy, lw=1, c='blue')
 
@@ -400,24 +403,21 @@ def populate_glacier_with_metadata(glacier_name, n=50):
                 for gl_neighbor_id in list_cluster_RGIIds:
                     gl_neighbor_df = oggm_rgi_glaciers.loc[oggm_rgi_glaciers['RGIId'] == gl_neighbor_id]
                     gl_neighbor_geom = gl_neighbor_df['geometry'].item()  # glacier geometry Polygon
-                    #gl_neighbor_geom_ext = Polygon(gl_neighbor_geom.exterior)  # glacier geometry Polygon
-                    #ax1.plot(*gl_neighbor_geom.exterior.xy, lw=1, c='orange', zorder=0)
+                    ax1.plot(*gl_neighbor_geom.exterior.xy, lw=1, c='orange', zorder=0)
 
-            # Plot intersections of central glacier
+            # Plot intersections of central glacier with its neighbors
             for k, intersect in enumerate(gl_intersects['geometry']):  # Linestring gl_intersects
-                continue
-                ax1.plot(*intersect.xy, lw=1, color='k', zorder=0)
+                ax1.plot(*intersect.xy, lw=1, color='k')
 
             # Plot intersections of all glaciers in the cluster
             if cluster_intersects is not None:
                 for k, intersect in enumerate(cluster_intersects['geometry']):
-                    continue
-                    ax1.plot(*intersect.xy, lw=1, color=np.random.rand(3), zorder=2)
+                    ax1.plot(*intersect.xy, lw=1, color='k') #np.random.rand(3)
 
-            # Plot cluster (with no ice divides)
-            ax1.plot(*cluster_geometry_no_divides_4326.item().exterior.xy, lw=1, c='red', zorder=3)
-            for interior in cluster_geometry_no_divides_4326.item().interiors:
-                ax1.plot(*interior.xy, lw=1, c='blue', zorder=3)
+                # Plot cluster ice divides removed
+                ax1.plot(*cluster_geometry_no_divides_4326.item().exterior.xy, lw=1, c='red', zorder=3)
+                for interior in cluster_geometry_no_divides_4326.item().interiors:
+                    ax1.plot(*interior.xy, lw=1, c='blue', zorder=3)
 
             if nunatak: ax1.scatter(lon, lat, s=50, lw=2, c='b')
             else: ax1.scatter(lon, lat, s=50, lw=2, c='r', ec='r')
@@ -425,21 +425,20 @@ def populate_glacier_with_metadata(glacier_name, n=50):
             ax2.plot(*geoseries_geometries_epsg.loc[0].xy, lw=1, c='red') # first entry is outside border
             for inter in geoseries_geometries_epsg.loc[1:]: # all interiors if present
                 ax2.plot(*inter.xy, lw=1, c='blue')
-            ax2.scatter(*point_epsg.xy, s=50, lw=2, c='r', ec='r')
+            if nunatak: ax2.scatter(*point_epsg.xy, s=50, lw=2, c='b')
+            else: ax2.scatter(*point_epsg.xy, s=50, lw=2, c='r', ec='r')
             if debug_distance: ax2.scatter(*nearest_point_on_line.xy, s=50, lw=2, c='g')
 
             ax1.set_title('EPSG 4326')
             ax2.set_title(f'EPSG {glacier_epsg}')
             plt.show()
-    print(f"Finished distance from border calculations in {time.time()-t0} sec.")
-
+    print(f"Finished distance calculations.")
 
     # Show the result
-    show_glacier_with_produced_points = False
+    show_glacier_with_produced_points = True
     if show_glacier_with_produced_points:
         fig, axes = plt.subplots(1,3, figsize=(8,4))
         ax1, ax2, ax3 = axes.flatten()
-
         for ax in (ax1, ax2):
             ax.plot(*gl_geom_ext.exterior.xy, lw=1, c='red')
             for interior in gl_geom.interiors:
@@ -467,17 +466,23 @@ def populate_glacier_with_metadata(glacier_name, n=50):
         s3_1 = ax3.scatter(x=lons_crs[np.argwhere(np.isnan(vx_data))], y=lats_crs[np.argwhere(np.isnan(vx_data))], s=50,
                            c='magenta', zorder=1)
 
-
         plt.show()
 
-    # todo: remove any nan line (can be in vx, vy, v, ith_m) ?
-    # print(points_df.T)
+
+    """ Cleaning the produced dataset """
+    # At this stage any nan may be present in vx, vy, v, ith_m (even if nunatak=0)
+    # Remove all lines having any nan in vx, vy, v. This will also automatically delete points inside nunataks.
+    points_df = points_df.dropna(subset=['vx', 'vy', 'v'])
+    print(points_df.T)
     return points_df
 
-df_points = populate_glacier_with_metadata(glacier_name='RGI60-11.00846', n=10)
+
+generated_points_dataframe = populate_glacier_with_metadata(glacier_name='RGI60-11.01450', n=10)
+
 #RGI60-08.00001
 # RGI60-11.00846 has multiple intersects with neighbors
-# RGI60-11.02774 has 1 intersect with neighbor (?)
+# RGI60-11.02774 has no neighbors
 #RGI60-11.02884 has no neighbors
 #glacier_name =  'RGI60-11.01450' Aletsch # RGI60-11.02774
 #glacier_name = np.random.choice(RGI_burned)
+#RGI60-11.00590, RGI60-11.01894 no Millan data ?
