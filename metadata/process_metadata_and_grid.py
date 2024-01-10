1641d27a2ae63b3a4716da78d021d6c0039a45b5
nico <nico.maffe89@gmail.com>
2024-01-10 02:08:54 +0100

    updates

--- metadata/process_metadata_and_grid.py
+++ metadata/process_metadata_and_grid.py
@@ -1,108 +1,126 @@
-import math
+import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import geopandas as gpd
 from scipy import stats
+import matplotlib.pyplot as plt
+from pandas.testing import assert_frame_equal
 
-PATH_METADATA_INPUT = '/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final.csv'
-glathida = pd.read_csv(PATH_METADATA_INPUT, low_memory=False)
-print(list(glathida))
+"""
+This program imports the generated metadata dataset from create_metadata.py and:
+1. Processing:
+    - Calculated the extra features: v, slope, elevation_from_zmin
+    - Remove old measurements
+    - Remove possible bad data
+    - Retain only some final features and remove any nan.
+2. Gridding:
+    - gridding is done computing the mean of each feature in all pixels that form the grid, which is specified by
+    the parameter --nbins_grid_latlon (default=20)
+    
+The processed and gridded dataframe is finally saved.
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_metadata_file', type=str,
+                    default="/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final.csv",
+                    help="Input metadata file to be gridded")
+parser.add_argument('--nbins_grid_latlon', type=float, default=20, help="How many bins in the lat/lon directions")
+args = parser.parse_args()
+
+run_compare = False
+if run_compare:
+    f1 = "/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final_grid_200.csv"
+    f2 = "/home/nico/PycharmProjects/skynet/Extra_Data/glathida/glathida-3.1.0/glathida-3.1.0/data/TTT_final_grid_200_old.csv"
+    df1 = pd.read_csv(f1, low_memory=False)
+    df2 = pd.read_csv(f2, low_memory=False)
+    print(df1.equals(df2))
+    print(df1.compare(df2))
+    exit()
+
+
+""" Import ungridded dataset """
+glathida = pd.read_csv(args.input_metadata_file, low_memory=False)
 
 """ A. Work on the dataset """
-
 # A.1 Add features
 glathida['v'] = np.sqrt(glathida['vx']**2 + glathida['vy']**2)
 glathida['slope'] = np.sqrt(glathida['slope_lat']**2 + glathida['slope_lon']**2)
 glathida['elevation_from_zmin'] = glathida['elevation_astergdem'] - glathida['Zmin']
 
-# A.2 Select only regions of interest, remove old (-er than 2005) measurements
-# and erroneous data (if DATA_FLAG is not nan)
-rgis = [3,7,8,11]
+# A.2 Remove old (-er than 2005) measurements and erroneous data (if DATA_FLAG is not nan)
+cond = ((glathida['SURVEY_DATE'] > 20050000) & (glathida['DATA_FLAG'].isna()) & (glathida['THICKNESS']>=0))
 
-cond = ((glathida['RGI'].isin(rgis)) & (glathida['SURVEY_DATE'] > 20050000)
-        & (glathida['DATA_FLAG'].isna()) & (glathida['THICKNESS']>=0)) #& (glathida['ith_m']>0)
-
-glathida_rgis = glathida[cond]
-print(f'Original columns: {list(glathida_rgis)} \n')
+glathida = glathida[cond]
+print(f'Original columns: {list(glathida)} \n')
 
 # A.3 Keep only these columns
 cols = ['RGI', 'RGIId', 'POINT_LAT', 'POINT_LON', 'THICKNESS', 'Area', 'elevation_astergdem',
         'slope_lat', 'slope_lon', 'vx', 'vy', 'dist_from_border_km', 'v', 'slope',
        'Zmin', 'Zmax', 'Zmed', 'Slope', 'Lmax', 'elevation_from_zmin', 'ith_m', 'ith_f']
 
-glathida_rgis = glathida_rgis[cols]
-print(f'We keep only the following columns: \n {list(glathida_rgis)} \n{len(glathida_rgis)} rows')
+glathida = glathida[cols]
+print(f'We keep only the following columns: \n {list(glathida)} \n{len(glathida)} rows')
 
 # A.4 Remove nans
-glathida_rgis = glathida_rgis.dropna(subset=cols)
-#print(glathida_rgis.isna().sum())
-#print(f'After having removed nans we have {len(glathida_rgis)} rows')
-#print(glathida_rgis['RGI'].value_counts())
+glathida = glathida.dropna(subset=cols)
+#print(glathida.isna().sum())
+#print(f'After having removed nans we have {len(glathida)} rows')
+#print(glathida['RGI'].value_counts())
 
 """ B. Grid the dataset """
 # We loop over all unique glacier ids; for each unique glacier we grid every feature.
-print(f"Now we grid. \n")
-ids_rgiid = glathida_rgis['RGIId'].unique().tolist()
-print(f'We have {len(ids_rgiid)} unique glaciers')
+print(f"Begin gridding.")
+ids_rgiid = glathida['RGIId'].unique().tolist()
+print(f'We have {len(ids_rgiid)} unique glaciers and {len(glathida)} rows')
 
-glathida_rgis_gridded = pd.DataFrame(columns=glathida_rgis.columns)
+glathida_gridded = pd.DataFrame(columns=glathida.columns)
 
 features_to_grid = ['THICKNESS', 'Area', 'slope_lat', 'slope_lon', 'elevation_astergdem', 'vx', 'vy',
                     'dist_from_border_km', 'v', 'slope', 'Zmin', 'Zmax', 'Zmed', 'Slope', 'Lmax',
                     'elevation_from_zmin', 'ith_m', 'ith_f']
 
-num_grid_lon_lat = 20
-n_measurements = []
-n_measurements_new = []
+list_num_measurements_before_grid = []
+list_num_measurements_after_grid = []
 
 # loop over unique glaciers
-for n, idrgi in enumerate(ids_rgiid):
+for n, rgiid in enumerate(ids_rgiid):
 
-    glathida_id = glathida_rgis.loc[glathida_rgis['RGIId'] == idrgi]
+    glathida_id = glathida.loc[glathida['RGIId'] == rgiid]
     glathida_id_grid = pd.DataFrame(columns=glathida_id.columns)
 
     lons = glathida_id['POINT_LON'].to_numpy()
     lats = glathida_id['POINT_LAT'].to_numpy()
-    gl_area = glathida_id['Area'].mean()
-    gl_nmeas = len(glathida_id)
-    gl_density = gl_nmeas / gl_area  # #/km2
+    rgi = glathida_id['RGI'].iloc[0]
 
-    # control
-    if (glathida_id['Area'].std() > 1.):
-        print(glathida_id['Area'].std())
-        input('PROBLEM')
+    # make same checks
+    if not glathida_id['Area'].nunique() == 1: raise ValueError(f"Glacier {rgiid} should have only 1 unique Area.")
+    if not glathida_id['RGI'].nunique() == 1: raise ValueError(f"Glacier {rgiid} should have only 1 unique RGI.")
+    if not glathida_id['RGIId'].nunique() == 1: raise ValueError(f"Glacier {rgiid} should have only 1 unique RGIId.")
 
-    print(f'{n}/{len(ids_rgiid)}, {idrgi}, Tot. meas: {gl_nmeas}')
+    print(f'{n}/{len(ids_rgiid)}, {rgiid}, Tot. meas to be gridded: {len(glathida_id)}')
 
-    n_measurements.append(gl_nmeas)
+    list_num_measurements_before_grid.append(len(glathida_id))
 
-    # if only one measurement, append that line
-    if (gl_nmeas == 1):
-        glathida_rgis_gridded = glathida_rgis_gridded.append(glathida_id) #todo: deprecated use .concat instead
-        n_measurements_new.append(gl_nmeas)
+    # if only one measurement, append that line as is
+    if (len(glathida_id) == 1):
+        glathida_gridded = pd.concat([glathida_gridded, glathida_id], ignore_index=True)
+        list_num_measurements_after_grid.append(len(glathida_id))
         continue
 
     # if more than one measurement, calcule the rectangular domain for gridding
     min_lat, max_lat = np.min(lats), np.max(lats)
     min_lon, max_lon = np.min(lons), np.max(lons)
-    binsx = np.arange(min_lon-1e-4, max_lon+1e-4, (max_lon-min_lon)/num_grid_lon_lat)
-    binsy = np.arange(min_lat-1e-4, max_lat+1e-4, (max_lat-min_lat)/num_grid_lon_lat)
+    binsx = np.arange(min_lon-1e-4, max_lon+1e-4, (max_lon-min_lon)/args.nbins_grid_latlon)
+    binsy = np.arange(min_lat-1e-4, max_lat+1e-4, (max_lat-min_lat)/args.nbins_grid_latlon)
 
-    # loop over features
+    # loop over each feature and grid
     for feature in features_to_grid:
 
-        vtest = glathida_id[feature].to_numpy()
-        is_nan = np.isnan(np.sum(vtest))
-        if is_nan: print('Watch out, nan in feature vector')
+        feature_array = glathida_id[feature].to_numpy()
+        if np.isnan(feature_array).any(): raise ValueError('Watch out, nan in feature vector')
 
         # grid the feature
-        H, xedges, yedges, binnumber = stats.binned_statistic_2d(x=lons, y=lats, values=vtest,
-                                                                 statistic=np.nanmean,
-                                                                 bins=[binsx, binsy])
-
-        # check how many values we have produced
-        new_gl_nmeas = np.count_nonzero(~np.isnan(H))
+        H, xedges, yedges, binnumber = stats.binned_statistic_2d(x=lons, y=lats, values=feature_array,
+                                                                 statistic=np.nanmean, bins=[binsx, binsy])
 
         # calculate the latutide and longitude of the grid
         xcenters = (xedges[:-1] + xedges[1:]) / 2
@@ -113,18 +131,14 @@ for n, idrgi in enumerate(ids_rgiid):
         ys = ycenters[y_indices]
         zs = H[non_nan_mask]
 
+        # check how many values we have produced
+        new_gl_nmeas = np.count_nonzero(~np.isnan(H))
         # print(feature, new_gl_nmeas, H.shape, xedges.shape, xs.shape, ys.shape, zs.shape)
 
-        # Fill gridded dataset
-        rgi = glathida_id['RGI'].iloc[0]
-        rgiid = glathida_id['RGIId'].iloc[0]
-        assert rgiid==idrgi, "These should be the same"
-
-        glathida_id_grid['POINT_LON'] = xs
-        glathida_id_grid['POINT_LAT'] = ys
+        # Fill gridded feature
+        glathida_id_grid['POINT_LON'] = xs  # unnecessarily overwriting each loop
+        glathida_id_grid['POINT_LAT'] = ys  # unnecessarily overwriting each loop
         glathida_id_grid[feature] = zs
-        glathida_id_grid['RGI'] = np.repeat(rgi, len(zs))  # check
-        glathida_id_grid['RGIId'] = np.repeat(rgiid, len(zs))  # check
 
         # plot
         ifplot = False
@@ -132,7 +146,7 @@ for n, idrgi in enumerate(ids_rgiid):
 
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
 
-            s1 = ax1.scatter(x=lons, y=lats, c=vtest, s=1)
+            s1 = ax1.scatter(x=lons, y=lats, c=feature_array, s=1)
             cbar1 = plt.colorbar(s1, ax=ax1, alpha=1)
             cbar1.set_label(feature, labelpad=15, rotation=270)
 
@@ -148,14 +162,21 @@ for n, idrgi in enumerate(ids_rgiid):
                 ax2.axhline(y_edge, color='gray', linestyle='--', linewidth=0.1)
             plt.show()
 
+    # add these two features as well
+    glathida_id_grid['RGI'] = np.repeat(rgi, len(glathida_id_grid))
+    glathida_id_grid['RGIId'] = np.repeat(rgiid, len(glathida_id_grid))
+
     # append glacier gridded dataset to main gridded dataset
-    glathida_rgis_gridded = glathida_rgis_gridded.append(glathida_id_grid) #todo: deprecated use .concat instead
+    glathida_gridded = pd.concat([glathida_gridded, glathida_id_grid], ignore_index=True)
 
-    n_measurements_new.append(len(glathida_id_grid))
+    list_num_measurements_after_grid.append(len(glathida_id_grid))
 
-ifsave = False
+print(f'Finished. No. original measurements {len(glathida)} down to {len(glathida_gridded)}.')
+
+ifsave = True
 if ifsave:
-    glathida_rgis_gridded.to_csv(PATH_METADATA_INPUT.replace('TTT_final.csv',
-                                                 f'TTT_final_grid_{num_grid_lon_lat}_new.csv'), index=False)
+    filename_out = args.input_metadata_file.replace('TTT_final.csv', f'TTT_final_grid_{args.nbins_grid_latlon}.csv')
+    glathida_gridded.to_csv(filename_out, index=False)
+    print(f"Gridded dataframe saved: {filename_out}.")
 
-print(f'Finished. No. original measurements {len(glathida_rgis)} down to {len(glathida_rgis_gridded)}.')
+
