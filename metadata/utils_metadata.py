daaa6f22cac2f96e96be135562cdc2f5783f57d6
nico <nico.maffe89@gmail.com>
2024-03-10 20:50:58 +0100

    added haversine

--- metadata/utils_metadata.py
+++ metadata/utils_metadata.py
@@ -1,7 +1,23 @@
 import utm
 import scipy
+from math import radians, cos, sin, asin, sqrt
 import numpy as np
 
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 def from_lat_lon_to_utm_and_epsg(lat, lon):
     """https://github.com/Turbo87/utm"""
