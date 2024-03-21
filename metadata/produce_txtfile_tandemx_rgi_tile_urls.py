2cb98ff3f283560e71151c499af1f34b18640ea8
nico <nico.maffe89@gmail.com>
2024-03-21 23:28:24 +0100

    minor updates

--- metadata/produce_txtfile_tandemx_rgi_tile_urls.py
+++ metadata/produce_txtfile_tandemx_rgi_tile_urls.py
@@ -11,7 +11,8 @@ from create_rgi_mosaic_tanxedem import get_NS, get_EW, min_closest_multiple, max
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--outfolder', type=str, default="/media/nico/samsung_nvme/Tandem-X-EDEM/", help="Outpath for saved files")
-parser.add_argument('--rgi', type=int, default=1, help="Region to produce list of url")
+parser.add_argument('--rgi', type=int, default=8, help="Region to produce list of url")
+parser.add_argument('--buffer', type=float, default=1./8, help="Buffer to glacier extent")
 parser.add_argument('--save', type=int, default=0, help="Save it or go home")
 
 args = parser.parse_args()
@@ -19,7 +20,7 @@ args = parser.parse_args()
 utils.get_rgi_dir(version='62')
 
 rgi = args.rgi
-buffer = 1./8
+buffer = args.buffer
 
 list_rgi_tiles = []
 list_rgi_tiles_url = []
@@ -47,14 +48,12 @@ for idx, row in gdf.iterrows():
 list_rgi_tiles  = list(set(list_rgi_tiles))  # Remove duplicates
 print(f"In rgi {rgi} we have produced {len(list_rgi_tiles)} tiles.")
 
-# We now need to format them as Tandem-X wants the names for download
+# We now need to format them as Tandem-X wants the names for download:
 #https://download.geoservice.dlr.de/TDM30_EDEM/files/TDM1_EDEM_10_N45/TDM1_EDEM_10_N45E010/TDM1_EDEM_10_N45E013_V01_C/TDM1_EDEM_10_N45E013_V01_C.zip
 for code in list_rgi_tiles:
     NSlat = code[0:3]
     EWlon = code[3:7]
     EWlon_mul10 = EWlon[:-1] + '0'
-    #lon = int(EWlon[1:4])
-    #lon_mul10 = (lon // 10) * 10
 
     # Create url and append to list
     file_code = f"https://download.geoservice.dlr.de/TDM30_EDEM/files/TDM1_EDEM_10_{NSlat}/TDM1_EDEM_10_{NSlat}{EWlon_mul10}" \
@@ -63,7 +62,7 @@ for code in list_rgi_tiles:
     #print(code, NSlat, EWlon, EWlon_mul10, file_code)
 
 print(f"Finished rgi {rgi}.")
-#print(list_rgi_tiles_url)
+#for i in list_rgi_tiles_url: print(i)
 
 # Save to txt
 if args.save:
@@ -71,14 +70,14 @@ if args.save:
     np.savetxt(f'{args.outfolder}TDM30_EDEM-url-list-rgi-{args.rgi}.txt', list_rgi_tiles_url, fmt='%s')
 
 # Extract the maximum x value from the limits
-plot=False
+plot=True
 if plot:
     min_lon, max_lon = min(ax.get_xlim()), max(ax.get_xlim())
     min_lon, max_lon = int(min_lon), int(np.ceil(max_lon))
     min_lat, max_lat = min(ax.get_ylim()), max(ax.get_ylim())
     min_lat, max_lat = int(min_lat), int(np.ceil(max_lat))
-    print(min_lon, max_lon)
-    print(min_lat, max_lat)
+    #print(min_lon, max_lon)
+    #print(min_lat, max_lat)
     ax.vlines(np.arange(min_lon, max_lon), ymin=min_lat, ymax=max_lat, color='grey', linestyle='-', alpha=0.5)
     ax.hlines(np.arange(min_lat, max_lat), xmin=min_lon, xmax=max_lon, color='grey', linestyle='-', alpha=0.5)
     plt.xticks(range(min_lon, max_lon + 1))
