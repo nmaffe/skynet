e35aae792a752514fde9c622812ce53aa2f58fa9
nmaffe <nmaffezz@uci.edu>
2024-06-04 11:19:29 -0700

    replace math with numpy to work with arrays

--- metadata/utils_metadata.py
+++ metadata/utils_metadata.py
@@ -1,7 +1,6 @@
 import utm
 import scipy
 import math
-from math import radians, cos, sin, asin, sqrt
 import numpy as np
 
 def haversine(lon1, lat1, lon2, lat2):
@@ -10,14 +9,14 @@ def haversine(lon1, lat1, lon2, lat2):
     on the earth (specified in decimal degrees)
     """
     # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
 
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Determines return value units.
     return c * r
 
 def from_lat_lon_to_utm_and_epsg(lat, lon):
